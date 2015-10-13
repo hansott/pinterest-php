c383e017e2f16c258b38902b9fa394414a92c45d
turanct <spinnewebber_toon@hotmail.com>
2015-10-13 11:25:19 +0200

    Match up Request and RequestInterface with our needs
    - Every method that's used on a Request (in e.g. GuzzleClient::execute)
      should be documented in its interface, otherwise we can't replace it
      with another implementation (or if we don't want do to that, we
      should just get rid of the interface and use the implementation
      everywhere)
    
    - Added missing phpdoc statements in both interface and implementation

--- src/Pinterest/Http/RequestInterface.php
+++ src/Pinterest/Http/RequestInterface.php
@@ -9,9 +9,45 @@ namespace Pinterest\Http;
  */
 interface RequestInterface
 {
+    /**
+     * Get the Http method
+     *
+     * @return string
+     */
     public function getMethod();
 
+    /**
+     * Get the Http endpoint
+     *
+     * @return string
+     */
     public function getEndpoint();
 
+    /**
+     * Get the Http parameters
+     *
+     * @return array
+     */
     public function getParams();
+
+    /**
+     * Get the Http headers
+     *
+     * @return array
+     */
+    public function getHeaders();
+
+    /**
+     * Is this a POST request?
+     *
+     * @return bool
+     */
+    public function isPost();
+
+    /**
+     * Is this a GET request?
+     *
+     * @return bool
+     */
+    public function isGet();
 }
